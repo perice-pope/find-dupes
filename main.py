ddb8239b8d737ef3524a866b0a1dfb36235eacc9
Perice Pope <perice09@gmail.com>
2021-10-09 00:01:52 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# Write a function to check if the list has duplicate elements. 
+
+list_ = [1, 2, 3, 4, 4, 5]
+list_2 = []
+def dup_check(list_): 
+  for i in list_: 
+    if i not in list_2:
+      list_2.append(i)
+      print(list_2)
+    else: 
+      print('There are dupes!') 
+
+dup_check(list_)
